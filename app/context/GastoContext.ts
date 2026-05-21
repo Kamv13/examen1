eb25042e6a1268821ecea3f8f266f55b5953ba7e
Kevin Mendoza <ksmv233@gmail.com>
2026-05-20 18:26:51 -0600

    models, context

---
+++ app/context/GastoContext.ts
@@ -0,0 +1,25 @@
+'use client'
+'use client';
+
+import { createContext } from "react";
+import { Gasto } from "../modelos/Gasto";
+
+export const GastoContext = createContext({
+
+  isLoggedIn: false,
+  login: (usuario: string, clave: string): boolean => false,
+  logout: () => {},
+
+
+  presupuesto: 0,
+  setPresupuesto: (monto: number) => {},
+  totalGastado: 0,
+
+
+  gastos: [] as Gasto[],
+  categorias: [] as string[],
+  agregarCategoria: (categoria: string) => {},
+  agregarGasto: (categoria: string, monto: number, fecha: string) => {},
+  eliminarGasto: (id: number) => {},
+  cargarGastos: () => {},
+});
